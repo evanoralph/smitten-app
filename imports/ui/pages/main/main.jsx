1fde073fe59bcfafb0cac56d0ba2744ce24ff554
immanuelv <immanuel.valle@gmail.com>
2018-04-11 15:25:55 +0800

    Added variables scss Added carousel sample

--- imports/ui/pages/main/main.jsx
+++ imports/ui/pages/main/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Carousel from '../../components/main/Carousel';
 
 class Main extends React.Component {
   render() {
     return (
       <div id="landing-page">
-        <div className="row">
-          <div className="center-xs">
+        <div className="row center-xs">
+          <div className="col-xs-12">
             <h2>HELLO SMITTEN</h2>
+            <Carousel/>
           </div>
         </div>
       </div>
