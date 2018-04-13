e38d10cba9278b284f5b3893819190a98c94bd51
SynthFX <immanuel.valle@gmail.com>
2018-04-13 22:52:11 +0800

    Added prototype calls for login and signup processes

--- imports/ui/components/main/login_form/LoginForm.jsx
+++ imports/ui/components/main/login_form/LoginForm.jsx
@@ -2,52 +2,81 @@ import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
 import Button from 'react-validation/build/button';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { required, password, email, notReq } from '../../../../lib/validations';
-import serialize from 'form-serialize';
+
+import { required, email } from '../../../../lib/validations';
+import { getFormData } from '../../../../lib/helpers';
 
 class LoginForm extends React.Component {
   loginDefault(e) {
     e.preventDefault();
-    const form = document.querySelector('#login-form');
-    const loginData = serialize(form, { hash: true });
+    const loginData = getFormData('#login-form');
 
     this.props.loginDefault(loginData);
   }
 
+  loginWithFacebook(e) {
+    e.preventDefault();
+    this.props.loginWithFacebook();
+  }
+
   render() {
     return (
-      <Form id="login-form"
-            ref={ c => this.form = c }
-            onSubmit={this.loginDefault.bind(this)}>
-        <div>
-          <Input
-            autoComplete="off"
-            className="form-input text-center"
-            value=""
-            name="email"
-            placeholder="E-Mail"
-            validations={[email, required]}
-          />
-        </div>
+      <div className="row center-xs">
+        <Form id="login-form"
+              ref={ c => this.form = c }
+              onSubmit={this.loginDefault.bind(this)}>
+          <div>
+            <Input
+              autoComplete="off"
+              className="form-input text-center"
+              value=""
+              name="email"
+              placeholder="E-Mail"
+              validations={[email, required]}
+            />
+          </div>
 
-        <div className="mg-top-15">
-          <Input
-            autoComplete="off"
-            className="form-input text-center"
-            value=""
-            name="password"
-            type="password"
-            placeholder="Password"
-            validations={[required]}
-          />
-        </div>
+          <div className="mg-top-15">
+            <Input
+              autoComplete="off"
+              className="form-input text-center"
+              value=""
+              name="password"
+              type="password"
+              placeholder="Password"
+              validations={[required]}
+            />
+          </div>
 
-        <Button className="btn-pink-rounded">
-          <i className="fa fa-fw fa-2x fa-envelope-o"></i>
-          <span>LOG IN</span>
-        </Button>
-      </Form>
+          <Button className="btn-pink-rounded">
+            <i className="fa fa-fw fa-2x fa-envelope-o"></i>
+            <span>LOG IN</span>
+          </Button>
+        </Form>
+        <div className="login-controls mg-top-10">
+          <Link to="/forgot-password" className="text-purple">
+            <small>Forgot your password?</small>
+          </Link>
+          <div className="no-margin">
+            <p className="text-white ">Log In With</p>
+            <button
+              onClick={this.loginWithFacebook.bind(this)}
+              type="button"
+              className="btn-facebook">
+              <i className="fa fa-fw fa-facebook-square fa-2x"></i>
+              <span>FACEBOOK</span>
+            </button>
+          </div>
+          <div className="mg-top-10">
+            <small className="text-white">Don't have an account? </small>
+            <Link to="/sign-up" className="text-purple text-bold">
+              <small>SIGN UP</small>
+            </Link>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- imports/ui/components/sign_up/sign_up_form/SignupForm.jsx
+++ imports/ui/components/sign_up/sign_up_form/SignupForm.jsx
@@ -2,39 +2,78 @@ import Form from 'react-validation/build/form';
 import Input from 'react-validation/build/input';
 import Button from 'react-validation/build/button';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { required, password, email } from '../../../../lib/validations';
+import { getFormData } from '../../../../lib/helpers';
 
 class SignupForm extends React.Component {
+  signUpDefault(e) {
+    e.preventDefault();
+    const signupData = getFormData('#signup-form');
+    this.props.signUpDefault(signupData);
+  }
+
+  signUpWithFacebook(e) {
+    e.preventDefault();
+    this.props.signUpWithFacebook();
+  }
+
   render() {
     return (
-      <Form id="signup-form">
-        <div>
-          <Input
-            className="form-input text-center"
-            value=""
-            name="email"
-            placeholder="E-Mail"
-            validations={[email]}
-          />
-        </div>
+      <div className="row center-xs">
+        <Form id="signup-form" onSubmit={this.signUpDefault.bind(this)}>
+          <div>
+            <Input
+              autoComplete="off"
+              className="form-input text-center"
+              value=""
+              name="email"
+              placeholder="E-Mail"
+              validations={[email, required]}
+            />
+          </div>
 
-        <div className="mg-top-15">
-          <Input
-            className="form-input text-center"
-            value=""
-            name="password"
-            type="password"
-            placeholder="Password"
-            validations={[required]}
-          />
-        </div>
+          <div className="mg-top-15">
+            <Input
+              autoComplete="off"
+              className="form-input text-center"
+              value=""
+              name="password"
+              type="password"
+              placeholder="Password"
+              validations={[required]}
+            />
+          </div>
 
-        <Button className="btn-pink-rounded">
-          <i className="fa fa-fw fa-2x fa-envelope-o"></i>
-          <span>SIGN UP</span>
-        </Button>
-      </Form>
+          <Button className="btn-pink-rounded">
+            <i className="fa fa-fw fa-2x fa-envelope-o"></i>
+            <span>SIGN UP</span>
+          </Button>
+        </Form>
+        <div className="signup-controls mg-top-10">
+          <Link to="/forgot-password" className="text-purple">
+            <small>Forgot your password?</small>
+          </Link>
+          <div className="no-margin">
+            <p className="text-white ">Sign Up With</p>
+            <button
+              onClick={this.signUpWithFacebook.bind(this)}
+              type="button"
+              className="btn-facebook">
+              <i className="fa fa-fw fa-facebook-square fa-2x"></i>
+              <span>FACEBOOK</span>
+            </button>
+          </div>
+          <div className="mg-top-10">
+            <small className="text-white">Don't have an account? </small>
+            <Link to="/" className="text-purple text-bold">
+              <small>LOG IN</small>
+            </Link>
+          </div>
+        </div>
+      </div>
     );
   }
 }
